e6a78bcbd5e16f4453c886bb05613f1ffc6f3796
Andrey <85159503+shkarin-andrey@users.noreply.github.com>
2022-02-10 03:40:43 +0300

    Delete keys directory

--- keys/keys.dev.js
+++
@@ -1,8 +0,0 @@
-
-module.exports = {
-    MONGODB_URI: 'mongodb+srv://andrey:1Zfjcfc1SSpD8pUC@cluster0.d4st5.mongodb.net/shop',
-    SESSION_SECRET: 'some secret value',
-    SENDGRID_API_KEY: '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',
-    EMAIL_FROM: 'shkarinandrew@mail.ru',
-    BASE_URL: 'localhost:3000'
-}
